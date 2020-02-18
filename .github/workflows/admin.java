945499898d1e64fc056b77536dbbcd01ba4c9e75
Anusree09 <61188475+Anusree09@users.noreply.github.com>
2020-02-18 16:48:14 +0530

    Create admin.java

---
+++ .github/workflows/admin.java
@@ -0,0 +1,78 @@
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@Table(name = "admin")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Admin.findAll", query = "SELECT a FROM Admin a"),
+    @NamedQuery(name = "Admin.findByUsername", query = "SELECT a FROM Admin a WHERE a.username = :username"),
+    @NamedQuery(name = "Admin.findByPassword", query = "SELECT a FROM Admin a WHERE a.password = :password")})
+public class Admin implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "username")
+    private String username;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "password")
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(String username) {
+        this.username = username;
+    }
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public int hashCode() {
+        int hash = 0;
+        hash += (username != null ? username.hashCode() : 0);
+        return hash;
+    }
+
+
+    public boolean equals(Object object) {
+        // to check ID 
+        if (!(object instanceof Admin)) {
+            return false;
+        }
+        Admin other = (Admin) object;
+        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+            return false;
+        }
+        return true;
+    }
+
+
+    public String toString() {
+        return "entity.Admin[ username=" + username + " ]";
+    }
+    
+}
